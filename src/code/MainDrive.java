e076d7adaf4413ecac06cae09ba1bfbf9c2e9b80
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-08-30 15:43:23 +0900

    문제출제

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,7 +4,35 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
+		int[] questionNumArr = new int[3];
 		
+		for (int i = 0; i < questionNumArr.length; i++) {
+			
+			while (true) {
+				
+				int randomNum = (int) (Math.random() * 9 + 1);
+				
+				boolean isDuplOk = true;
+				
+				for (int num : questionNumArr) {
+					if (randomNum == num) {
+						isDuplOk = false;
+					}
+				}
+				
+				if (isDuplOk) {
+					questionNumArr[i] = randomNum;
+					
+					break;
+				}
+				
+			}
+			
+		}
+		
+		for (int num : questionNumArr) {
+			System.out.print(num + " ");
+		}
 		
 	}
 
