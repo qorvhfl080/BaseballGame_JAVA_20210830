457f179ab2ec45a26d6c9a0c3f3a98407989e293
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-08-30 16:07:42 +0900

    시도횟수 추가

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,6 +7,7 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 		int[] questionNumArr = new int[3];
+		int tries = 0;
 		
 		for (int i = 0; i < questionNumArr.length; i++) {
 			
@@ -34,9 +35,12 @@ public class MainDrive {
 		
 		Scanner sc = new Scanner(System.in);
 		int[] inputNumberArr = new int[3];
+		
 		while (true) {
 			System.out.print("3자리 숫자 입력 : ");
 			int num = sc.nextInt();
+			
+			tries++;
 			
 			inputNumberArr[0] = num / 100;
 			inputNumberArr[1] = (num % 100) / 10;
@@ -67,6 +71,7 @@ public class MainDrive {
 			
 			if (strikeCount == 3) {
 				System.out.println("정답입니다.");
+				System.out.println(tries + "번 시도했습니다.");
 				break;
 			}
 				
